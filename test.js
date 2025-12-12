cf9a70e690e6b42b1e2f9d72b23fdbe8ddf919e1
Naincy33 <iamnaincy33@gmail.com>
2025-12-12 23:58:27 +0530

    manipulation done

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
+// DYNAMIC DOM!
 let h1= document.querySelector("h1");
 h1.innerText= "hello hello, sab badhiya? ";
 console.log("h1");
 
 let h2= document.createElement("h2");
-h2.textContent= "Append hai to last me body ke andar!";
+h2.textContent= "Append hai to last me body ke andar! (h2)";
 document.body.append(h2);
 
 let h3= document.createElement("h3");
@@ -18,10 +19,24 @@ h4.textContent= "Ye h4 hai, h2 ke baad insert hua hai!";
 let h2Element= document.querySelector("h2");
 h2Element.insertAdjacentElement("afterend", h4);
 
-
+//STYLE UPDATE!
 h1.style.color= "brown";
 h1.style.backgroundColor= "beige";
 h1.style.border= "2px solid purple";
-//h1.classList.add("big-font");
+h1.classList.add("big-font");
+
+// ATTRIBUTE UPDATE!
+h1.setAttribute("title", "This is a heading tag");
+console.log(h1.getAttribute("title"));
+
+// LIST ITEMS UPDATE!
+let ul= document.querySelector("ul");
+let li= document.createElement("li");
+li.textContent= "JavaScript DOM Manipulation";
+ul.prepend(li);
+
+
+div.setAttribute("id", "my-div");
+div.setAttribute("class", "container");
 
 
